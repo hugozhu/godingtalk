b9a60259fd896c839a06551515116b1819679548
Athurg Gooth <athurg@gooth.org>
2016-07-11 17:08:07 +0800

    分离底层的HTTP请求自动添加AccessToken的代码

--- transport.go
+++ transport.go
@@ -28,14 +28,18 @@ type DownloadFile struct {
 }
 
 func (c *DingTalkClient) httpRPC(path string, params url.Values, requestData interface{}, responseData Unmarshallable) error {
-	client := c.HTTPClient
-	var request *http.Request
 	if c.AccessToken != "" {
 		if params == nil {
 			params = url.Values{}
 		}
 		params.Set("access_token", c.AccessToken)
 	}
+	return c.httpRequest(path, params, requestData, responseData)
+}
+
+func (c *DingTalkClient) httpRequest(path string, params url.Values, requestData interface{}, responseData Unmarshallable) error {
+	client := c.HTTPClient
+	var request *http.Request
 	url2 := ROOT + path + "?" + params.Encode()
 	if requestData != nil {
 		switch requestData.(type) {
