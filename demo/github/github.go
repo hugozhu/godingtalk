dad33648ed83556c78db36417da1e2c2b0cc1954
Hugo Zhu <hugozhu@gmail.com>
2020-03-03 20:07:51 +0800

    Removed memcache on google appengine

--- demo/github/github.go
+++ demo/github/github.go
@@ -7,11 +7,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
-	"google.golang.org/appengine/memcache"
 
 	"strings"
 
@@ -48,7 +46,7 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 	// 	Context: context,
 	// 	AllowInvalidServerCertificate: true,
 	// }
-	c.Cache = NewMemCache(context, ".access_token")
+	c.Cache = NewZeroCache(context, ".access_token")
 	refresh_token_error := c.RefreshAccessToken()
 
 	msg := godingtalk.OAMessage{}
@@ -125,42 +123,22 @@ func verifySignature(secret []byte, signature string, body []byte) bool {
 	return hmac.Equal(signBody(secret, body), actual)
 }
 
-type MemCache struct {
+type ZeroCache struct {
 	ctx context.Context
 	key string
 }
 
-func NewMemCache(ctx context.Context, key string) *MemCache {
-	return &MemCache{
+func NewZeroCache(ctx context.Context, key string) *ZeroCache {
+	return &ZeroCache{
 		ctx: ctx,
 		key: key,
 	}
 }
 
-func (c *MemCache) Set(data godingtalk.Expirable) error {
-	bytes, err := json.Marshal(data)
-	if err == nil {
-		item := &memcache.Item{
-			Key:        c.key,
-			Value:      bytes,
-			Expiration: time.Duration(data.ExpiresIn()) * time.Second,
-		}
-		err = memcache.Set(c.ctx, item)
-	}
-	return err
+func (c *ZeroCache) Set(data godingtalk.Expirable) error {
+	return nil
 }
 
-func (c *MemCache) Get(data godingtalk.Expirable) error {
-	item, err := memcache.Get(c.ctx, c.key)
-	if err == nil {
-		err = json.Unmarshal(item.Value, data)
-		if err == nil {
-			created := data.CreatedAt()
-			expires := data.ExpiresIn()
-			if err == nil && time.Now().Unix() > created+int64(expires-60) {
-				err = errors.New("Data is already expired")
-			}
-		}
-	}
-	return err
+func (c *ZeroCache) Get(data godingtalk.Expirable) error {
+	return errors.New("Not found")
 }
